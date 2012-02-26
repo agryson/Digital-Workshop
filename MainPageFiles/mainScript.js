305599ea5d86c568657ad80112d09d8d3e54d38c
agryson <agryson@gmail.com>
2012-02-26 20:42:27 +0000

    "Complete overhaul of site, still lots of work on the js needed"

--- MainPageFiles/mainScript.js
+++ MainPageFiles/mainScript.js
@@ -1,77 +1,47 @@
-var projectsWOpen = 950;
-var plansWOpen = 900;
-var linksWOpen = 850;
-var projectsWClosed = 150;
-var plansWClosed = 100;
-var linksWClosed = 50;
+/*The panels*/
+var panel1;
+var panel2;
+var panel3;
+var panel4;
+var panel5;
+var panel6;
+var panel7;
 
-var esc; //listen for excape key
+/*Track if a panel is open or not*/
+var panel1Open = false;
+var panel2Open = false;
+var panel3Open = false;
+var panel4Open = false;
+var panel5Open = false;
+var panel6Open = false;
+var panel7Open = false;
+
+/*a panelArray starts with its number, id, odd? and open?*/
+
+var panelsArray = [];
+
+var bump = 290;
+
 
 function init() {
-    document.onkeydown = keydown;
+    panel1 = document.getElementById("panel1");
+    panel2 = document.getElementById("panel2");
+    panel3 = document.getElementById("panel3");
+    panel4 = document.getElementById("panel4");
+    panel5 = document.getElementById("panel5");
+    panel6 = document.getElementById("panel6");
+    panel7 = document.getElementById("panel7");
+    panelsArray = [[1,panel1,true,false],[2,panel2,false,false],[3,panel3,true,false],[4,panel4,false,false],[5,panel5,true,false],[6,panel6,false,false],[7,panel7,true,false]];
 }
 
-var openAbout = function() {
-    if (document.getElementById("projects").style.width > (projectsWClosed + "px")) {
-		document.getElementById("projects").style.width = projectsWClosed + "px";
-	}
-	if (document.getElementById("plans").style.width > (plansWClosed + "px")){
-		document.getElementById("plans").style.width = plansWClosed + "px";
-	}
-	if (document.getElementById("links").style.width > (linksWClosed + "px")) {
-		document.getElementById("links").style.width = linksWClosed + "px";
-	}
-};
-
-var openProjects = function(obj) {
-	obj.style.width = projectsWOpen + "px";
-	if (document.getElementById("plans").style.width > (plansWClosed + "px")){
-		document.getElementById("plans").style.width = plansWClosed + "px";
-	}
-	if (document.getElementById("links").style.width > (linksWClosed + "px")) {
-		document.getElementById("links").style.width = linksWClosed + "px";
-	}
-};
-
-var openPlans = function(obj) {
-	obj.style.width = plansWOpen + "px";
-	if (document.getElementById("projects").style.width < (projectsWOpen + "px")){
-		document.getElementById("projects").style.width = projectsWOpen + "px";
-	} 
-	if (document.getElementById("links").style.width > (linksWClosed + "px")) {
-		document.getElementById("links").style.width = linksWClosed + "px";
-	}
-};
-
-var openLinks = function(obj) {
-	obj.style.width = linksWOpen + "px";
-	if (document.getElementById("projects").style.width < projectsWOpen + "px"){
-		document.getElementById("projects").style.width = "950px";
-	}
-	if (document.getElementById("plans").style.width < plansWOpen + "px") {
-		document.getElementById("plans").style.width = "900px";
-	}
-};
-
-var popOut = function(id) {
-	console.log("called id " + id);
-	document.getElementById("popBack").style.display = "inherit";
-	document.getElementById("popBox").style.display = "block";
-    switch(id) {
-        case document.getElementById("pongThumb"):
-            document.getElementById("popBox").innerHTML = "<iframe id='pongFrame' src='../Pong/pong.html' scrolling='no'></iframe>";
-            break;
-        default:
-        
+function expand(panel) {
+    console.log(panel + panelsArray[1][0]);
+    console.log("panel + panelsArray[i][0]");
+    var i;
+    for(i=0; i<7; i++) { //loops through the array
+        if(panel === panelsArray[i][1]){ //locks onto the panel we've been passed
+            console.log(panel + panelsArray[i][0]);
+        }
     }
-};
-
-function keydown(e) {
-    if (e.keyCode === 27) {
-        popAway();
-    }
-}
-function popAway() {
-    document.getElementById("popBack").style.display = "none";
-    document.getElementById("popBox").style.display = "none";
+    
 }
